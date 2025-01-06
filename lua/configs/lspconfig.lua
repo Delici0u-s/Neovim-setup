e3b5df5cf53c981ad278e1a52a6979c6b7bfeec1
Delici0u-s <maxi.bravo111@gmail.com>
2025-01-06 15:38:16 +0100

    added python to lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "clangd" }
+local servers = { "html", "cssls", "clangd", "pyright" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 nvlsp.defaults() -- loads nvchad's defaults
@@ -12,32 +12,32 @@ nvlsp.defaults() -- loads nvchad's defaults
 -- lsps with default config
 
 -- Older
--- for _, lsp in ipairs(servers) do
---   lspconfig[lsp].setup {
---     on_attach = nvlsp.on_attach,
---     on_init = nvlsp.on_init,
---     capabilities = nvlsp.capabilities,
---   }
--- end
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
 
 --New V
-for _, lsp in ipairs(servers) do
-  if lsp == "clangd" then
-    -- Custom configuration for clangd
-    lspconfig[lsp].setup {
-      on_attach = nvlsp.on_attach,
-      on_init = nvlsp.on_init,
-      capabilities = nvlsp.capabilities,
-      -- cmd = { "clangd", "--std=c++23" }, -- Pass C++23 standard flag
-    }
-  else
-    lspconfig[lsp].setup {
-      on_attach = nvlsp.on_attach,
-      on_init = nvlsp.on_init,
-      capabilities = nvlsp.capabilities,
-    }
-  end
-end
+-- for _, lsp in ipairs(servers) do
+--   if lsp == "clangd" then
+--     -- Custom configuration for clangd
+--     lspconfig[lsp].setup {
+--       on_attach = nvlsp.on_attach,
+--       on_init = nvlsp.on_init,
+--       capabilities = nvlsp.capabilities,
+--       -- cmd = { "clangd", "--std=c++23" }, -- Pass C++23 standard flag
+--     }
+--   else
+--     lspconfig[lsp].setup {
+--       on_attach = nvlsp.on_attach,
+--       on_init = nvlsp.on_init,
+--       capabilities = nvlsp.capabilities,
+--     }
+--   end
+-- end
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
 --   on_attach = nvlsp.on_attach,
